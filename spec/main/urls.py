5b326a9ba821e309102d453c8011938fa87705c8
Clément Habinshuti <haby_habbes@outlook.com>
2016-06-29 08:00:46 +0000

    Route url to home page

--- spec/main/urls.py
+++ spec/main/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import HomeView
 
 urlpatterns = [
-    url(r'^/$', HomeView.as_view(), 'home')    
+    url(r'^$', HomeView.as_view(), name='home')    
 ]
