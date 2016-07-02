8fc4621cf97b2f530d18003538b5977b0a3c273c
Habbes <haby_habbes@outlook.com>
2016-07-02 18:52:36 +0300

    Setup custom auth pipeline

---
+++ spec/specauth/pipeline.py
@@ -0,0 +1,25 @@
+"""
+Functions to plug into the social auth pipeline
+"""
+
+
+def queue_init_profile(backend, user, response, social, *args, **kwargs):
+    """
+    queue request to initialize profile data if this is a new user
+    or new association for an existing user
+    :param backend:
+    :param user:
+    :param response:
+    :param social:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    provider = backend.name
+    data = social.extra_data
+    token, uid = data['access_token'], data['id']
+    print('provider', provider)
+    print('social', social)
+    print('data', data)
+    print('args', kwargs)
+
