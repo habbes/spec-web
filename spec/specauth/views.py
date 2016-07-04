e391c7b2b7dc0b10db6722c3deb30d493b79d816
Habbes <haby_habbes@outlook.com>
2016-07-04 10:17:19 +0300

    Show user profile link on navbar if authenticated

--- spec/specauth/views.py
+++ spec/specauth/views.py
@@ -3,5 +3,14 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+
 class AuthView(TemplateView):
     template_name = 'auth/index.html'
+
+    def get_context_date(self, **kwargs):
+        ctx = (AuthView, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user:
+            ctx['user'] = user
+            ctx['profile'] = user.profile
+        return ctx
