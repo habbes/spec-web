1b936164736481196672f43bec97b3aa12bbeb09
Habbes <haby_habbes@outlook.com>
2016-07-04 03:59:39 +0300

    Create profile models

--- spec/main/models.py
+++ spec/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill)
+    
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    homepage = models.CharField(max_length=255, default='')
+    url = models.CharField(max_length=255, default='')
+    description = models.TextField(default='')
+    provider = models.CharField(max_length=255)
+    external_id = models.CharField(max_length=255)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s: %s' % (self.provider, self.name)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+
