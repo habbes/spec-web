22482b3a7d79d5c710b84f2ed5085272b1cfd33b
Habbes <haby_habbes@outlook.com>
2016-07-04 11:38:22 +0300

    Implement logout

--- spec/specauth/urls.py
+++ spec/specauth/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import AuthView
+from .views import AuthView, logout_view
 
 urlpatterns = [
     url('^$', AuthView.as_view(), name='auth'),
+    url('^logout$', logout_view, name='logout')
 ]
