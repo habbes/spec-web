dedf8299838e7b9a931d71f2a40eeb1fe94143b6
Habbes <haby_habbes@outlook.com>
2016-07-27 05:42:16 +0300

    Add featured profiles on homepage

--- spec/main/views.py
+++ spec/main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Profile
 
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(HomeView, self).get_context_data(**kwargs)
+        ctx['featured_profiles'] = Profile.objects.all()[:3]
+        return ctx
